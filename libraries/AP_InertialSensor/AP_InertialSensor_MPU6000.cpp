4aaead7345a3c3f06266567d9e9f4aea9e651979
Pat Hickey <pat@galois.com>
2011-11-12 16:01:16 -0800

    Orientation change for Purple hardware. Rotated so PWM out in rear.
    - Requested by Doug Weibel, Tridge, Jose Julio. Typically easier to put servo
      outputs in rear in aircraft.
    - Aligned the accel and gyro signs with the APM1 according to the
      following diagram:
    
                      Front
        APM1                      Purple
         (leds)                 (leds) (pwm in)
        --------                  ---------
        |      |                  |       |
        |      |                  |USB    |
        |      |                  |       |
        |      |                  |       |
        |USB   |                  |       |
        --------                  ---------
        (pwm pins)                (pwm out)
                      Rear

--- libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_MPU6000.cpp
@@ -73,10 +73,10 @@ const float AP_InertialSensor_MPU6000::_accel_scale = 9.81 / 4096.0;
  *      but somone else should please confirm.
  */
 const uint8_t AP_InertialSensor_MPU6000::_gyro_data_index[3]  = { 5, 4, 6 };
-const int8_t  AP_InertialSensor_MPU6000::_gyro_data_sign[3]   = { -1, -1, -1 };
+const int8_t  AP_InertialSensor_MPU6000::_gyro_data_sign[3]   = { 1, 1, -1 };
 
 const uint8_t AP_InertialSensor_MPU6000::_accel_data_index[3] = { 1, 0, 2 };
-const int8_t  AP_InertialSensor_MPU6000::_accel_data_sign[3]  = { -1, -1, -1 };
+const int8_t  AP_InertialSensor_MPU6000::_accel_data_sign[3]  = { 1, 1, -1 };
 
 const uint8_t AP_InertialSensor_MPU6000::_temp_data_index = 3;
 
