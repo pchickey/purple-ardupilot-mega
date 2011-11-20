be031715151ec036b51c786941173194926532bc
Pat Hickey <pat@galois.com>
2011-11-19 22:10:32 -0800

    Added ArduCopter_Motors header files.

---
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Octa_Quad.h
@@ -0,0 +1,17 @@
+
+#ifndef __ARDUCOPTER_MOTORS_OCTA_QUAD_H__
+#define __ARDUCOPTER_MOTORS_OCTA_QUAD_H__
+
+#include "ArduCopter_Motors.h"
+
+class ArduCopter_Motors_Octa_Quad : public ArduCopter_Motors
+{
+  public:
+  void init_out();
+  void output_armed();
+  void output_disarmed();
+  void output_test();
+};
+
+#endif // __ARDUCOPTER_MOTORS_OCTA_QUAD_H__
+
