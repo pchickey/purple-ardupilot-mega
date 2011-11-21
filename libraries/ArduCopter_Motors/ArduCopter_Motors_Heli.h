db778cecbbb9ddc475c64cb1f9bfeff8531fc20b
Pat Hickey <pat@galois.com>
2011-11-20 16:24:53 -0800

    ArduCopter_Motors: add APM_RC_Class constructor parameter
    * each constructor now takes APM_RC_Class *
    * each class now has private variable APM_RC_Class *_apm_rc

--- libraries/ArduCopter_Motors/ArduCopter_Motors_Heli.h
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Heli.h
@@ -7,10 +7,15 @@
 class ArduCopter_Motors_Heli : public ArduCopter_Motors
 {
   public:
+  ArduCopter_Motors_Heli(APM_RC_Class *apm_rc) :
+    _apm_rc(apm_rc) {}
+
   void init_out();
   void output_armed();
   void output_disarmed();
   void output_test();
+  private:
+  APM_RC_Class *_apm_rc;
 };
 
 #endif // __ARDUCOPTER_MOTORS_HELI_H__
--- libraries/ArduCopter_Motors/ArduCopter_Motors_Hexa.h
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Hexa.h
@@ -7,10 +7,15 @@
 class ArduCopter_Motors_Hexa : public ArduCopter_Motors
 {
   public:
+  ArduCopter_Motors_Hexa(APM_RC_Class *apm_rc) :
+    _apm_rc(apm_rc) {}
+
   void init_out();
   void output_armed();
   void output_disarmed();
   void output_test();
+  private:
+  APM_RC_Class *_apm_rc;
 };
 
 #endif // __ARDUCOPTER_MOTORS_HEXA_H__
