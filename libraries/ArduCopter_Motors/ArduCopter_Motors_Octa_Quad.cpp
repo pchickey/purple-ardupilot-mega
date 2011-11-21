e1087f9469d5f4a9a32d2ff238d275ecafcaf97d
Pat Hickey <pat@galois.com>
2011-11-20 17:05:54 -0800

    ArduCopter_Motors : each constructor adds int16_t *motor_out param
    Each class also has ivar int16_t *_motor_out
    Each class method renames motor_out[] accesses to _motor_out[] accesses.

--- libraries/ArduCopter_Motors/ArduCopter_Motors_Octa_Quad.cpp
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Octa_Quad.cpp
@@ -32,20 +32,20 @@ void ArduCopter_Motors_Octa_Quad::output_armed()
 		pitch_out 	 	= (float)g.rc_2.pwm_out * .707;
 
         // Front Left
-		motor_out[CH_7]    = ((g.rc_3.radio_out * g.top_bottom_ratio) + roll_out + pitch_out);  // CCW TOP
-	    motor_out[CH_8]	   =  g.rc_3.radio_out + roll_out + pitch_out;			        // CW
+		_motor_out[CH_7]    = ((g.rc_3.radio_out * g.top_bottom_ratio) + roll_out + pitch_out);  // CCW TOP
+	    _motor_out[CH_8]	   =  g.rc_3.radio_out + roll_out + pitch_out;			        // CW
 
         // Front Right
-		motor_out[CH_10]	= ((g.rc_3.radio_out * g.top_bottom_ratio) - roll_out + pitch_out);	// CCW TOP
-	    motor_out[CH_11]	=  g.rc_3.radio_out - roll_out + pitch_out;			        // CW
+		_motor_out[CH_10]	= ((g.rc_3.radio_out * g.top_bottom_ratio) - roll_out + pitch_out);	// CCW TOP
+	    _motor_out[CH_11]	=  g.rc_3.radio_out - roll_out + pitch_out;			        // CW
 
 		// Back Left
-		motor_out[CH_3]		= ((g.rc_3.radio_out * g.top_bottom_ratio) + roll_out - pitch_out);	// CCW TOP
-	    motor_out[CH_4]		=  g.rc_3.radio_out + roll_out - pitch_out;			        // CW
+		_motor_out[CH_3]		= ((g.rc_3.radio_out * g.top_bottom_ratio) + roll_out - pitch_out);	// CCW TOP
+	    _motor_out[CH_4]		=  g.rc_3.radio_out + roll_out - pitch_out;			        // CW
 
 		// Back Right
-		motor_out[CH_1]		= ((g.rc_3.radio_out * g.top_bottom_ratio) - roll_out - pitch_out);	// CCW TOP
-	    motor_out[CH_2]		=  g.rc_3.radio_out - roll_out - pitch_out;			        // CW
+		_motor_out[CH_1]		= ((g.rc_3.radio_out * g.top_bottom_ratio) - roll_out - pitch_out);	// CCW TOP
+	    _motor_out[CH_2]		=  g.rc_3.radio_out - roll_out - pitch_out;			        // CW
 
 
 
@@ -54,76 +54,76 @@ void ArduCopter_Motors_Octa_Quad::output_armed()
 		pitch_out 	 	= g.rc_2.pwm_out;
 
 		 // Left
-		motor_out[CH_7]    = (g.rc_3.radio_out * g.top_bottom_ratio) - roll_out;   // CCW TOP
-	    motor_out[CH_8]		=  g.rc_3.radio_out - roll_out;			        // CW
+		_motor_out[CH_7]    = (g.rc_3.radio_out * g.top_bottom_ratio) - roll_out;   // CCW TOP
+	    _motor_out[CH_8]		=  g.rc_3.radio_out - roll_out;			        // CW
 
         // Right
-		motor_out[CH_1]		= (g.rc_3.radio_out * g.top_bottom_ratio) + roll_out;	// CCW TOP
-	    motor_out[CH_2]		=  g.rc_3.radio_out + roll_out;		            // CW
+		_motor_out[CH_1]		= (g.rc_3.radio_out * g.top_bottom_ratio) + roll_out;	// CCW TOP
+	    _motor_out[CH_2]		=  g.rc_3.radio_out + roll_out;		            // CW
 
 		// Front
-		motor_out[CH_10]		= (g.rc_3.radio_out * g.top_bottom_ratio) + pitch_out;	// CCW TOP
-	    motor_out[CH_11]		=  g.rc_3.radio_out + pitch_out;			    // CW
+		_motor_out[CH_10]		= (g.rc_3.radio_out * g.top_bottom_ratio) + pitch_out;	// CCW TOP
+	    _motor_out[CH_11]		=  g.rc_3.radio_out + pitch_out;			    // CW
 
 		// Back
-		motor_out[CH_3]		= (g.rc_3.radio_out * g.top_bottom_ratio) - pitch_out;	// CCW TOP
-	    motor_out[CH_4]		=  g.rc_3.radio_out - pitch_out;			    // CW
+		_motor_out[CH_3]		= (g.rc_3.radio_out * g.top_bottom_ratio) - pitch_out;	// CCW TOP
+	    _motor_out[CH_4]		=  g.rc_3.radio_out - pitch_out;			    // CW
 
 	}
 
 	// Yaw
-	motor_out[CH_1]		+= g.rc_4.pwm_out;	// CCW
-	motor_out[CH_3]		+= g.rc_4.pwm_out;	// CCW
-	motor_out[CH_7] 	+= g.rc_4.pwm_out;	// CCW
-	motor_out[CH_10] 	+= g.rc_4.pwm_out;	// CCW
+	_motor_out[CH_1]		+= g.rc_4.pwm_out;	// CCW
+	_motor_out[CH_3]		+= g.rc_4.pwm_out;	// CCW
+	_motor_out[CH_7] 	+= g.rc_4.pwm_out;	// CCW
+	_motor_out[CH_10] 	+= g.rc_4.pwm_out;	// CCW
 
-	motor_out[CH_2]		-= g.rc_4.pwm_out;	// CW
-	motor_out[CH_4]		-= g.rc_4.pwm_out;	// CW
-	motor_out[CH_8]	    -= g.rc_4.pwm_out;	// CW
-	motor_out[CH_11]    -= g.rc_4.pwm_out;	// CW
+	_motor_out[CH_2]		-= g.rc_4.pwm_out;	// CW
+	_motor_out[CH_4]		-= g.rc_4.pwm_out;	// CW
+	_motor_out[CH_8]	    -= g.rc_4.pwm_out;	// CW
+	_motor_out[CH_11]    -= g.rc_4.pwm_out;	// CW
 
 	// TODO add stability patch
-	motor_out[CH_1]		= min(motor_out[CH_1], 	out_max);
-	motor_out[CH_2]		= min(motor_out[CH_2], 	out_max);
-	motor_out[CH_3]		= min(motor_out[CH_3], 	out_max);
-	motor_out[CH_4]		= min(motor_out[CH_4], 	out_max);
-	motor_out[CH_7]		= min(motor_out[CH_7],  out_max);
-	motor_out[CH_8]		= min(motor_out[CH_8],  out_max);
-	motor_out[CH_10]	= min(motor_out[CH_10], out_max);
-	motor_out[CH_11] 	= min(motor_out[CH_11], out_max);
+	_motor_out[CH_1]		= min(_motor_out[CH_1], 	out_max);
+	_motor_out[CH_2]		= min(_motor_out[CH_2], 	out_max);
+	_motor_out[CH_3]		= min(_motor_out[CH_3], 	out_max);
+	_motor_out[CH_4]		= min(_motor_out[CH_4], 	out_max);
+	_motor_out[CH_7]		= min(_motor_out[CH_7],  out_max);
+	_motor_out[CH_8]		= min(_motor_out[CH_8],  out_max);
+	_motor_out[CH_10]	= min(_motor_out[CH_10], out_max);
+	_motor_out[CH_11] 	= min(_motor_out[CH_11], out_max);
 
 	// limit output so motors don't stop
-	motor_out[CH_1]		= max(motor_out[CH_1], 	out_min);
-	motor_out[CH_2]		= max(motor_out[CH_2], 	out_min);
-	motor_out[CH_3]		= max(motor_out[CH_3], 	out_min);
-	motor_out[CH_4] 	= max(motor_out[CH_4], 	out_min);
-	motor_out[CH_7]		= max(motor_out[CH_7], 	out_min);
-	motor_out[CH_8] 	= max(motor_out[CH_8], 	out_min);
-	motor_out[CH_10]	= max(motor_out[CH_10], out_min);
-	motor_out[CH_11] 	= max(motor_out[CH_11], out_min);
+	_motor_out[CH_1]		= max(_motor_out[CH_1], 	out_min);
+	_motor_out[CH_2]		= max(_motor_out[CH_2], 	out_min);
+	_motor_out[CH_3]		= max(_motor_out[CH_3], 	out_min);
+	_motor_out[CH_4] 	= max(_motor_out[CH_4], 	out_min);
+	_motor_out[CH_7]		= max(_motor_out[CH_7], 	out_min);
+	_motor_out[CH_8] 	= max(_motor_out[CH_8], 	out_min);
+	_motor_out[CH_10]	= max(_motor_out[CH_10], out_min);
+	_motor_out[CH_11] 	= max(_motor_out[CH_11], out_min);
 
 	#if CUT_MOTORS == ENABLED
 	// if we are not sending a throttle output, we cut the motors
 	if(g.rc_3.servo_out == 0){
-		motor_out[CH_1]		= g.rc_3.radio_min;
-		motor_out[CH_2]		= g.rc_3.radio_min;
-		motor_out[CH_3]		= g.rc_3.radio_min;
-		motor_out[CH_4] 	= g.rc_3.radio_min;
-		motor_out[CH_7] 	= g.rc_3.radio_min;
-		motor_out[CH_8] 	= g.rc_3.radio_min;
-		motor_out[CH_10] 	= g.rc_3.radio_min;
-		motor_out[CH_11] 	= g.rc_3.radio_min;
+		_motor_out[CH_1]		= g.rc_3.radio_min;
+		_motor_out[CH_2]		= g.rc_3.radio_min;
+		_motor_out[CH_3]		= g.rc_3.radio_min;
+		_motor_out[CH_4] 	= g.rc_3.radio_min;
+		_motor_out[CH_7] 	= g.rc_3.radio_min;
+		_motor_out[CH_8] 	= g.rc_3.radio_min;
+		_motor_out[CH_10] 	= g.rc_3.radio_min;
+		_motor_out[CH_11] 	= g.rc_3.radio_min;
 	}
 	#endif
 
-	_apm_rc->OutputCh(CH_1, motor_out[CH_1]);
-	_apm_rc->OutputCh(CH_2, motor_out[CH_2]);
-	_apm_rc->OutputCh(CH_3, motor_out[CH_3]);
-	_apm_rc->OutputCh(CH_4, motor_out[CH_4]);
-	_apm_rc->OutputCh(CH_7, motor_out[CH_7]);
-	_apm_rc->OutputCh(CH_8, motor_out[CH_8]);
-	_apm_rc->OutputCh(CH_10, motor_out[CH_10]);
-	_apm_rc->OutputCh(CH_11, motor_out[CH_11]);
+	_apm_rc->OutputCh(CH_1, _motor_out[CH_1]);
+	_apm_rc->OutputCh(CH_2, _motor_out[CH_2]);
+	_apm_rc->OutputCh(CH_3, _motor_out[CH_3]);
+	_apm_rc->OutputCh(CH_4, _motor_out[CH_4]);
+	_apm_rc->OutputCh(CH_7, _motor_out[CH_7]);
+	_apm_rc->OutputCh(CH_8, _motor_out[CH_8]);
+	_apm_rc->OutputCh(CH_10, _motor_out[CH_10]);
+	_apm_rc->OutputCh(CH_11, _motor_out[CH_11]);
 
 	#if INSTANT_PWM == 1
 	// InstantPWM
@@ -141,9 +141,9 @@ void ArduCopter_Motors_Octa_Quad::output_disarmed()
 		motor_auto_armed = true;
 	}
 
-	// fill the motor_out[] array for HIL use
+	// fill the _motor_out[] array for HIL use
 	for (unsigned char i = 0; i < 11; i++) {
-		motor_out[i] = g.rc_3.radio_min;
+		_motor_out[i] = g.rc_3.radio_min;
 	}
 
 	// Send commands to motors
