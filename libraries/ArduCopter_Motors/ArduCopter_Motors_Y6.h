012a9d552493cd240fa52dcf12cf79a631d19afc
Pat Hickey <pat@galois.com>
2011-11-20 16:24:53 -0800

    ArduCopter_Motors: add APM_RC_Class constructor parameter
    * each constructor now takes APM_RC_Class *
    * each class now has private variable APM_RC_Class *_apm_rc

--- libraries/ArduCopter_Motors/ArduCopter_Motors_Y6.h
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Y6.h
@@ -7,10 +7,15 @@
 class ArduCopter_Motors_Y6 : public ArduCopter_Motors
 {
   public:
+  ArduCopter_Motors_Y6(APM_RC_Class *apm_rc) :
+    _apm_rc(apm_rc) {}
+
   void init_out();
   void output_armed();
   void output_disarmed();
   void output_test();
+  private:
+  APM_RC_Class *_apm_rc;
 };
 
 #endif // __ARDUCOPTER_MOTORS_Y6_H__
