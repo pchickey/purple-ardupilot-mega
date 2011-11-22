b148ad1324747b3288608d23d871fed92bb3eb5c
Pat Hickey <pat@galois.com>
2011-11-21 19:20:43 -0800

    ArduCopter Motors: multicopter ArduCopter_Motors subclasses made into ArduCopter_Motors_Multicopter subclasses
    * add ArduCopter_Motors_Multicopter to class inheritance
    * change to constructors to have all members ArduCopter_Motors_Multicopter needs
    * removed private variables, which are provided by ArduCopter_Motors_Multicopter

--- libraries/ArduCopter_Motors/ArduCopter_Motors_Hexa.h
+++ libraries/ArduCopter_Motors/ArduCopter_Motors_Hexa.h
@@ -2,23 +2,24 @@
 #ifndef __ARDUCOPTER_MOTORS_HEXA_H__
 #define __ARDUCOPTER_MOTORS_HEXA_H__
 
-#include <stdint.h>
-#include "../APM_RC/APM_RC.h"
 #include "ArduCopter_Motors.h"
+#include "ArduCopter_Motors_Multicopter.h"
 
-class ArduCopter_Motors_Hexa : public ArduCopter_Motors
+class ArduCopter_Motors_Hexa :
+    public ArduCopter_Motors
+  , public ArduCopter_Motors_Multicopter
 {
   public:
-  ArduCopter_Motors_Hexa(APM_RC_Class *apm_rc, int16_t *motor_out) :
-    _apm_rc(apm_rc) , _motor_out(motor_out) {}
+  ArduCopter_Motors_Hexa(ArduCopter_Motors_MulticopterDelegate *g,
+                         APM_RC_Class *apm_rc ,
+                         int16_t *motor_out   ,
+                         boolean *auto_armed  ) :
+    ArduCopter_Motors_Multicopter(g, apm_rc, motor_out, auto_armed) {}
 
   void init_out();
   void output_armed();
   void output_disarmed();
   void output_test();
-  private:
-  APM_RC_Class *_apm_rc;
-  int16_t *_motor_out;
 };
 
 #endif // __ARDUCOPTER_MOTORS_HEXA_H__
