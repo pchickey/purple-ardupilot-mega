27a0e72f263b597c4aec590b83ab6355fab1f2dc
Pat Hickey <pat@galois.com>
2012-08-21 14:11:54 -0700

    fixes to build a basic stub AP_HAL_AVR w/ AVRUARTDriver

--- libraries/AP_HAL_AVR/UARTDriver.h
+++ libraries/AP_HAL_AVR/UARTDriver.h
@@ -14,14 +14,24 @@
 
 class AP_HAL_AVR::AVRUARTDriver : public AP_HAL::UARTDriver {
 public:
-    AVRUARTDriver(int num) : _num(num) {}
-    void init(uint16_t baud) { _baud = baud; }
-    /* XXX stub implementation! */
-private:
-    int      _num;
-    uint16_t _baud;
-};
+    AVRUARTDriver(int port) {}
+    /* Implementations of UARTDriver virtual methods */
+    void begin(long b) {}
+    void begin(long b, unsigned int rxS, unsigned int txS) {}
+    void end() {}
+    void flush() {}
 
+    /* Implementations of BetterStream virtual methods */
+    int txspace() { return 1; }
+
+    /* Implementations of Stream virtual methods */
+    int available() { return 0; }
+    int read() { return -1; }
+    int peek() { return -1; }
+
+    /* Implementations of Print virtual methods */
+    size_t write(uint8_t c) { return 0; }
+};
 
 #endif // __AP_HAL_AVR_UART_DRIVER_H__
 
