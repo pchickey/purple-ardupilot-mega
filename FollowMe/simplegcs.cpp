6453fdf2466651673c29b550e6c9fcb0c9e50deb
Pat Hickey <pat@moreproductive.org>
2012-10-02 11:48:51 -0700

    Followme: simplegcs extended to have async console flush and heartbeat
    * kind of a rough hack for now, just trying to prototype this out.

--- FollowMe/simplegcs.cpp
+++ FollowMe/simplegcs.cpp
@@ -6,6 +6,42 @@ extern const AP_HAL::HAL& hal;
 #include <GCS_Console.h>
 #include "simplegcs.h"
 
+extern mavlink_channel_t downstream_channel;
+
+static volatile uint8_t lock = 0;
+
+void simplegcs_send_console_async(uint32_t machtnichts) {
+  if (lock) return;
+  lock = 1;
+  gcs_console_send(downstream_channel);
+  lock = 0;
+}
+
+void simplegcs_send_heartbeat_async(uint32_t us) {
+  uint32_t ms = us / 1000;
+  static uint32_t last_ms = 0;
+  if (ms - last_ms < 1000) return;
+  if (lock) return;
+  last_ms = ms;
+  lock = 1;
+  {
+    uint8_t base_mode = MAV_MODE_FLAG_STABILIZE_ENABLED
+                      | MAV_MODE_FLAG_GUIDED_ENABLED
+                      | MAV_MODE_FLAG_SAFETY_ARMED
+                      | MAV_MODE_FLAG_CUSTOM_MODE_ENABLED;
+    uint8_t system_status = MAV_STATE_ACTIVE;
+    uint32_t custom_mode = 5 ; /* Loiter is mode 5. */
+    mavlink_msg_heartbeat_send(
+            downstream_channel,
+            MAV_TYPE_QUADROTOR,
+            MAV_AUTOPILOT_ARDUPILOTMEGA,
+            base_mode,
+            custom_mode,
+            system_status);
+  }
+  lock = 0;
+}
+
 void simplegcs_send_heartbeat(mavlink_channel_t chan) {
     uint8_t base_mode = MAV_MODE_FLAG_STABILIZE_ENABLED
                       | MAV_MODE_FLAG_GUIDED_ENABLED
@@ -14,6 +50,8 @@ void simplegcs_send_heartbeat(mavlink_channel_t chan) {
     uint8_t system_status = MAV_STATE_ACTIVE;
     uint32_t custom_mode = 5 ; /* Loiter is mode 5. */
 
+    while(lock);
+    lock = 1;
     mavlink_msg_heartbeat_send(
             chan,
             MAV_TYPE_QUADROTOR,
@@ -21,6 +59,7 @@ void simplegcs_send_heartbeat(mavlink_channel_t chan) {
             base_mode,
             custom_mode,
             system_status);
+    lock = 0;
 }
 
 
@@ -33,10 +72,13 @@ bool simplegcs_try_send_statustext(mavlink_channel_t chan, const char *text, int
     if (len < 50) {
         memcpy(statustext, text, len);
     }
+    while(lock);
+    lock = 1;
     mavlink_msg_statustext_send(
             chan,
             1, /* SEVERITY_LOW */
             statustext);
+    lock = 0;
     return true;
 }
 // -----------------------------------------------------------------------
--- FollowMe/simplegcs.h
+++ FollowMe/simplegcs.h
@@ -12,5 +12,8 @@ bool simplegcs_try_send_statustext(mavlink_channel_t chan, const char *text, int
 void simplegcs_update(mavlink_channel_t chan, simplegcs_handler_t);
 void handle_message(mavlink_channel_t chan, mavlink_message_t* msg);
 
+void simplegcs_send_console_async(uint32_t ms);
+void simplegcs_send_heartbeat_async(uint32_t ms);
+
 #endif // __SIMPLE_GCS_H__
 
