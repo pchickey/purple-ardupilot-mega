36ed1f1bec9404eecf71712d70aa63cb1185cc2d
Pat Hickey <pat@moreproductive.org>
2012-10-05 10:59:16 -0700

    FollowMe: State machine outline. No testing or hooks to app yet.

---
+++ FollowMe/state.cpp
@@ -0,0 +1,131 @@
+
+#include "state.h"
+#include <AP_HAL.h>
+
+extern const AP_HAL::HAL& hal;
+extern mavlink_channel_t upstream_channel;
+extern mavlink_channel_t downstream_channel;
+
+void FMStateMachine::on_upstream_command_long(mavlink_command_long_t* pkt) {
+  switch(pkt->command) {
+    case MAV_CMD_NAV_LOITER_UNLIM:
+    case MAV_CMD_NAV_RETURN_TO_LAUNCH:
+    case MAV_CMD_NAV_LAND:
+    case MAV_CMD_MISSION_START:
+      /* clear out FM control of vehicle */
+      _on_user_override();
+    break;
+    case MAV_CMD_PREFLIGHT_CALIBRATION:
+      /* i guess do nothing? */
+    break;
+  }
+}
+
+void FMStateMachine::on_upstream_set_mode(mavlink_set_mode_t* pkt) {
+  /* mode is set in pkt->custom_mode */
+  _vehicle_mode = (int8_t) pkt->custom_mode;
+  /* clear out FM control of vehicle */
+  _on_user_override();
+}
+
+void FMStateMachine::on_downstream_heartbeat(mavlink_heartbeat_t* pkt) {
+  /* if mode has changed from last set_mode, the user has triggered a change
+   * via RC switch.
+   * clear out FM control of vehicle */
+  bool pktarmed = ((pkt->base_mode & MAV_MODE_FLAG_SAFETY_ARMED) > 0);
+  int8_t pktmode = (int8_t) pkt->custom_mode;
+  if ((pktarmed != _vehicle_armed) || (pktmode != _vehicle_mode)) {
+    _on_user_override();
+  }
+  /* update heartbeat millis */
+  _last_vehicle_hb_millis = hal.scheduler->millis();
+  /* update local state */ 
+  _vehicle_armed = pktarmed;
+  _vehicle_mode = pktmode;
+}
+
+void FMStateMachine::on_downstream_global_position_int(mavlink_global_position_int_t* pkt) {
+  /* Keep track of vehicle's latest lat, lon, altitude */
+}
+
+void FMStateMachine::on_button_activate() {
+  if (_guiding) return;
+  /* This action is allowed to swing the state to start guide mode. */
+  if (_check_guide_valid()) {
+    _set_guide_offset();
+    _send_guide();
+    _guiding = true;
+    hal.console->println_P(PSTR("Button activated, entering guided mode"));
+  } else {
+    hal.console->println_P(PSTR("Button activated but insufficient conditions"
+          "for entering guided mode"));
+  }
+}
+
+void FMStateMachine::on_loop() {
+  uint32_t now = hal.scheduler->millis();
+  if (_last_run_millis + _loop_period < now) return;
+  _last_run_millis = now;
+
+  _update_local_gps();
+
+  if (_guiding) {
+    _send_guide();
+  }
+}
+
+bool FMStateMachine::_check_guide_valid() {
+  uint32_t now = hal.scheduler->millis();
+
+  bool vehicle_gps_valid = (_vehicle_gps_fix == 3);
+  bool vehicle_hb_valid = (now - _last_vehicle_hb_millis) < 2000;
+
+  bool vehicle_mode_valid = _vehicle_armed 
+                          && ( (_vehicle_mode == MODE_LOITER)
+                             ||(_vehicle_mode == MODE_ALT_HOLD)
+                             ||(_vehicle_mode == MODE_AUTO)
+                             );
+
+  return _local_gps_valid
+      && vehicle_gps_valid
+      && vehicle_hb_valid
+      && vehicle_mode_valid;
+}
+
+void FMStateMachine::_update_local_gps() {
+  /* Cause an on_fault_cancel if when local gps has transitioned form 
+   * valid to invalid. */
+  if (_local_gps_valid && !(_gps->status() == GPS::GPS_OK)) {
+    _on_fault_cancel();
+  } 
+
+  _local_gps_valid = (_gps->status() == GPS::GPS_OK);
+  if (_gps->new_data) {
+    _local_gps_lat      = _gps->latitude;
+    _local_gps_lat      = _gps->longitude;
+    _local_gps_altitude = _gps->altitude;
+
+  }
+}
+
+void FMStateMachine::_set_guide_offset() {
+  hal.console->println_P(PSTR("Placeholder: Should set guide mode offset here."));
+}
+
+void FMStateMachine::_on_fault_cancel() {
+  _send_loiter();
+  _guiding = false;
+}
+
+void FMStateMachine::_on_user_override() {
+  hal.console->println_P(PSTR("User GCS or RC override of FollowMe"));
+  _guiding = false;
+}
+
+void FMStateMachine::_send_guide() {
+  hal.console->println_P(PSTR("Placeholder: Send guide waypoint packet"));
+}
+
+void FMStateMachine::_send_loiter() {
+  hal.console->println_P(PSTR("Placeholder: Send setmode loiter packet"));
+}
---
+++ FollowMe/state.h
@@ -0,0 +1,82 @@
+
+#ifndef __FOLLOWME_STATE_H__
+#define __FOLLOWME_STATE_H__
+
+#include <GCS_MAVLink.h>
+#include <AP_GPS.h>
+
+#include "arducopter_defines.h"
+
+class FMStateMachine {
+public:
+  FMStateMachine(GPS* gps) :
+    _gps(gps),
+    _last_run_millis(0),
+    _loop_period(500),
+    _last_vehicle_hb_millis(0),
+    _vehicle_armed(false),
+    _vehicle_mode(MODE_NUM_MODES),
+    _vehicle_gps_fix(0),
+    _vehicle_lat(0),
+    _vehicle_lon(0),
+    _vehicle_altitude(0)
+  {}
+  
+  void on_downstream_heartbeat(mavlink_heartbeat_t *pkt);
+  void on_downstream_global_position_int(mavlink_global_position_int_t* pkt);
+
+  void on_upstream_command_long(mavlink_command_long_t *pkt);
+  void on_upstream_set_mode(mavlink_set_mode_t* pkt);
+
+  void on_loop();
+
+  void on_button_activate();
+  void on_button_cancel();
+
+private:
+  /* _send_guide: Send a guide waypoint packet upstream. */
+  void _send_guide();
+
+  /* _send_loiter: Send a setmode loiter packet upstream. */
+  void _send_loiter();
+
+  /* Calculate whether we have sufficient conditions to enter guide mode. */
+  bool _check_guide_valid();
+
+  /* _update_local_gps: Get device's current GPS status and location. Called
+   * periodically. Can activate _on_fault_cancel(); */
+  void _update_local_gps();
+
+  void _on_user_override();
+  void _on_fault_cancel();
+
+  void _set_guide_offset();
+
+  /* Set once a start guide mode packet has been sent.
+   * Unset whenever we stop guiding. */
+  bool _guiding;
+  /* */
+  GPS* _gps;
+
+  /* Scheduling the on_loop periodic updater. */
+  uint32_t _last_run_millis;
+  uint32_t _loop_period;
+  uint32_t _last_vehicle_hb_millis;
+
+  int8_t  _vehicle_mode;
+  bool    _vehicle_armed;
+  uint8_t _vehicle_gps_fix;
+  int32_t _vehicle_lat;
+  int32_t _vehicle_lon;
+  int32_t _vehicle_altitude;
+
+  /* If local gps becomes non-valid, and we're guiding, put the vehicle into
+   * loiter. */
+  bool _local_gps_valid;
+  int32_t _local_gps_lat;
+  int32_t _local_gps_lon;
+  int32_t _local_gps_altitude;
+};
+
+#endif // __FOLLOWME_STATE_H__
+
